363246301f16c2bb28f63501df3e33e7578f22a8
codepilot2010 <93812156+codepilot2010@users.noreply.github.com>
2022-11-19 19:38:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function startclassification()
+{
+    navigator.mediaDevices.getUserMedia({
+        audio:true
+    });
+    classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/e33ZumEuS/model.json',modelReady);
+}
+function modelReady()
+{
+    classifier.classify(gotResults);
+}
+function gotResults(error,results)
+{
+    console.log("got result");
+}
